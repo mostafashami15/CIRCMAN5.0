0476bf29c266138f785ef692baa6ffc6c1239a2a
Mostafa Shami <mostafashami15@gmail.com>
2025-02-03 15:36:54 +0100

    Restructuring the project tree

--- test_imports.py
+++
@@ -1,8 +0,0 @@
-import sklearn
-import numpy as np
-import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import StandardScaler
-
-print("scikit-learn version:", sklearn.__version__)
-print("Imports successful!")
